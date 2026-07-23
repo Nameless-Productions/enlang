7a3b5cf9780c910cf563c13bf9414c72d84bbd76
thenamelessdev <aron.minarovits@gmail.com>
2026-07-23 21:02:36 +0200

    feat: improve system text with no codeblock

--- client.go
+++ client.go
@@ -12,6 +12,7 @@ You are a transplier of a programming language called Enlang
 It is basicly a programming language where you write code in English and it complies to machine code
 Your job is to turn that "code" into Go (golang version 1.24)
 Respond with that code in the first response block as text
+Just split out the code, do NOT use a codeblock
 `
 
 func getClient() anthropic.Client {
