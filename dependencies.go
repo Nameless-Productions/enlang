5fb48b0caad4f29b3cb381b18a5d3d12256ccec6
thenamelessdev <aron.minarovits@gmail.com>
2026-07-23 23:25:44 +0200

    feat: install command

--- dependencies.go
+++ dependencies.go
@@ -2,12 +2,26 @@ package main
 
 import (
 	"log"
+	"os"
 	"os/exec"
 )
 
 func initProject() {
 	cmd := exec.Command("go", "mod", "init", "project");
 	_, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func installDependencie(name string) {
+	_, err := os.ReadFile("out/go.mod")
+	if err != nil {
+		initProject()
+	}
+
+	cmd := exec.Command("go", "get", name);
+	_, err = cmd.CombinedOutput()
 	if err != nil {
 		log.Fatal(err)
 	}
