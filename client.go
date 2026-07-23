03e895c276be42e68cce8d1410e91ffdaa912e55
thenamelessdev <aron.minarovits@gmail.com>
2026-07-23 20:46:19 +0200

    feat: system text

--- client.go
+++ client.go
@@ -7,6 +7,12 @@ import (
 	"github.com/anthropics/anthropic-sdk-go/option"
 )
 
+var systemTxt = `
+You are a transplier of a programming language called Enlang
+It is basicly a programming language where you write code in English and it complies to machine code
+Your job is to turn that "code" into Go (golang version 1.24)
+`
+
 func getClient() anthropic.Client {
 	return anthropic.NewClient(
 		option.WithAPIKey(os.Getenv("CLAUDE")),
