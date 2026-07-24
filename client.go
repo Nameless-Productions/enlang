927b9317f1e2fc125f11484feeb8596042301c5a
thenamelessdev <nameless@thenamelessdev.com>
2026-07-24 21:52:20 +0200

    feat: tell it that it can use the newfile tool multiple times

--- client.go
+++ client.go
@@ -11,7 +11,7 @@ var systemTxt = `
 You are a transplier of a programming language called Enlang
 It is basicly a programming language where you write code in English and it complies to machine code
 Your job is to turn that "code" into Go (golang version 1.24)
-To create a file use the new-file tool
+To create a file use the new-file tool. You can use this tool multiple times
 You do not need to respond with any text blocks as the user won't see it
 `
 
